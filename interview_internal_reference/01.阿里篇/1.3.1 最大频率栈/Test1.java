a9f47ce4ed94e459d5e94781a98ccde4e05eb90f
吴开云 <kaiyun.wu@hand-china.com>
2020-01-15 16:32:05 +0800

    [fix]编写实现最大频率栈

--- interview_internal_reference/01.阿里篇/1.3.1 最大频率栈/Test1.java
+++ interview_internal_reference/01.阿里篇/1.3.1 最大频率栈/Test1.java
@@ -4,7 +4,7 @@
  * @Date: 2019/10/14 0014
  * @Version： 1.0
  */
-public class Test {
+public class Test1 {
     public static void main(String[] args) {
         FreqStack freqStack = new FreqStack();
         freqStack.push(5);freqStack.push(7);freqStack.push(5);
